cd999499dea3470eae6b3c3b8117b3d57fffbf38
abdulhafizr <abdulhafizramadan.17@gmail.com>
2020-12-26 13:14:45 +0700

    learn method all and get

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+app.all('/users', (request, response, next) => {
+	response.send('users page');
+	next();		
+})
+
+app.get('/posts', (request, response) => {
+	response.send('posts page');
+})
+
 app.listen(port, (req, res) => {
-    console.log('Server is running!');
+    	console.log('Server is running!');
 })
