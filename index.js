3597636be4fa85a14317d7a04e730c72e301b5e4
abdulhafizr <abdulhafizramadan.17@gmail.com>
2020-12-26 20:32:13 +0700

    handle CORS error

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const usersRoutes = require('./src/routes/users');
 const port = 4000;
 
+app.use((request, response, next) => {
+	response.setHeader("Access-Control-Allow-Origin", "*");
+	response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+	response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	next();
+})
 app.use(usersRoutes);
 
 app.listen(port, () => {
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,37 @@
 module.exports =  userController = {
     index: (request, response) => {
-        response.send('all users');
+        response.json({
+            message: 'All users',
+            data: [
+                {
+                    name: 'abdul',
+                    age: 18
+                },
+                {
+                    name: 'hafiz',
+                    age: 18
+                },
+                {
+                    name: 'ramadan',
+                    age: 18
+                },
+            ],
+            method: request.method
+        });
     },
     show: (request, response) => {
         const userId = request.params.userId;
         response.send(`detail user with id : ${userId}`);
     },
     store: (request, response) => {
-        response.send('user has created');
+        response.json({
+            message: 'All users',
+            data: {
+                name: 'ucup',
+                age: 18
+            },
+            method: request.method
+        })
     },
     update: (request, response) => {
         const userId = request.params.userId;
